0e7badb4203b5709b74f4a09b5d9834778730f5a
Ali Rakhshanipur <alirakhshanipur@Alis-MacBook-Pro.local>
2022-02-07 13:54:55 +0330

    edited2

--- bin/khajavi/project/main.dart
+++ bin/khajavi/project/main.dart
@@ -2,20 +2,24 @@ import 'dart:io';
 import 'classes.dart';
 
 void main(List<String> args) {
-  // Circle obj_circle1 = new Circle(radius: 12);
-  // Circle obj_circle2 = new Circle(radius: 10);
-  // Square obj_square1 = new Square(latitude: 2.2);
-  // Square obj_square2 = new Square(latitude: 5);
+  Circle obj_circle1 = new Circle(radius: 12.0); //radius must be double
+  Circle obj_circle2 = new Circle(radius: 10.0); //radius must be double
+  Square obj_square1 = new Square(latitude: 2.2); //latitude must be double
+  Square obj_square2 = new Square(latitude: 5.0); //latitude must be double
   Triangle obj_Tri = new Triangle(
-      side1: 12.0, side2: 12.0, side3: 10.0, height: 10.0, isSidesEqual: false);
-  // print(obj_square1.circumference());
-  // print(obj_square2.circumference());
-  // print(obj_square1.area());
-  // print(obj_square2.area());
-  // print(obj_circle1.circumference());
-  // print(obj_circle2.circumference());
-  // print(obj_circle1.area());
-  // print(obj_circle2.area());
+      side1: 12.0,
+      side2: 12.0,
+      side3: 10.0,
+      height: 10.0,
+      isSidesEqual: false); //sides and height must be double
+  print(obj_square1.circumference());
+  print(obj_square2.circumference());
+  print(obj_square1.area());
+  print(obj_square2.area());
+  print(obj_circle1.circumference());
+  print(obj_circle2.circumference());
+  print(obj_circle1.area());
+  print(obj_circle2.area());
   print(obj_Tri.area());
   print(obj_Tri.circumference());
 }
